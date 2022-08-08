2cde5d8e5c977841a1341ee142193ddea9ecf23b
Peter Havens <peter.havens@gmail.com>
2022-08-08 10:06:36 -0600

    Setup basic routing to Home

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,19 @@
+import Home from "./routes/Home";
 import { Helmet } from "react-helmet-async";
+import { Routes, Route } from "react-router-dom";
+import { StrictMode } from "react";
 
 function App() {
   return (
-    <>
+    <StrictMode>
       <Helmet>
         <title>Meals</title>
       </Helmet>
 
-      <div>Meals</div>
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </StrictMode>
   );
 }
 
