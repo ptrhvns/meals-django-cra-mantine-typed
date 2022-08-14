43620929a7d7d0eeb50576583faf8e9af2ef9551
Peter Havens <peter.havens@gmail.com>
2022-08-14 12:59:56 -0600

    Add type annotation

--- api/config/settings.py
+++ api/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6pi!-93x6=ubtdu&z+&mkoyk0=a1xs^!t@#*r!n7h5plpscd^^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: list[str] = []
 
 
 # Application definition
