a1b6fed16515925667ac084b1cd0a79eb8dd6646
Peter Havens <peter.havens@gmail.com>
2022-08-25 09:02:23 -0600

    Add recipe destroy API

--- api/main/urls.py
+++ api/main/urls.py
@@ -7,6 +7,7 @@ urlpatterns: list[URLPattern | URLResolver] = [
     path("csrf_token/", views.csrf_token, name="csrf_token"),
     path("login/", views.login, name="login"),
     path("logout/", views.logout, name="logout"),
+    path("recipe/<int:recipe_id>/destroy", views.recipe_destroy, name="recipe_destroy"),
     path("recipe/create/", views.recipe_create, name="recipe_create"),
     path("recipes/", views.recipes, name="recipes"),
     path("signup/", views.signup, name="signup"),
---
+++ api/main/views/recipe_destroy.py
@@ -0,0 +1,16 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.request import Request
+from rest_framework.response import Response
+
+from main.models import Recipe
+
+
+@api_view(http_method_names=["POST"])
+@permission_classes([IsAuthenticated])  # type: ignore[list-item]
+def recipe_destroy(request: Request, recipe_id: int) -> Response:
+    recipe = get_object_or_404(Recipe, pk=recipe_id, user=request.user)
+    recipe.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
