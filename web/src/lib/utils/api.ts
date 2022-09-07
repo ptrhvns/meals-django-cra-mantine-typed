98c57eee5d8154598ae95b3a3e05879be57886eb
Peter Havens <peter.havens@gmail.com>
2022-09-07 11:19:50 -0600

    Add ability to process field errors

--- web/src/lib/utils/api.ts
+++ web/src/lib/utils/api.ts
@@ -1,11 +1,24 @@
 import { ApiResponse } from "../../hooks/useApi";
+import { forOwn, head } from "lodash";
+import { ReactNode } from "react";
 
 export function handledApiError(
   response: ApiResponse,
-  { setAlert }: { setAlert?: (s: string | undefined) => void }
+  {
+    setAlert,
+    setFieldError,
+  }: {
+    setAlert?: (s: string | undefined) => void;
+    setFieldError?: (path: string, error: ReactNode) => void;
+  }
 ): boolean {
   if (response.isError) {
     if (setAlert) setAlert(response.message);
+
+    if (setFieldError) {
+      forOwn(response.errors, (value, key) => setFieldError(key, head(value)));
+    }
+
     return true;
   }
 
