194771d66aec63304d7a5fcc36f201b81a2d513d
Peter Havens <peter.havens@gmail.com>
2022-09-14 13:05:37 -0600

    Remove unused code

--- api/main/models/token.py
+++ api/main/models/token.py
@@ -1,14 +1,7 @@
-from django.db.models import (CASCADE, CharField, DateTimeField, ForeignKey,
-                              Manager, Model)
-from django.db.models.query import QuerySet
+from django.db.models import CASCADE, CharField, DateTimeField, ForeignKey, Model
 
 from main.lib.tokens import build_token
 from main.models.user import User
-
-
-class TokenManager(Manager["Token"]):
-    def email_confirmations(self) -> QuerySet["Token"]:
-        return self.filter(token_type=self.model.EMAIL_CONFIRMATION)
 
 
 class Token(Model):
@@ -19,8 +12,6 @@ class Token(Model):
     token = CharField(max_length=256, default=build_token, unique=True)
     token_type = CharField(choices=TOKEN_TYPE_CHOICES, max_length=32)
     user: ForeignKey[User] = ForeignKey(User, on_delete=CASCADE, related_name="tokens")
-
-    objects = TokenManager()
 
     def __str__(self) -> str:
         return self.token
