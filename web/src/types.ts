125b04d536c404036e6b99308d49782daba062f4
Peter Havens <peter.havens@gmail.com>
2022-09-29 11:02:56 -0600

    Include equipment data, and expand types

--- web/src/types.ts
+++ web/src/types.ts
@@ -1,18 +1,29 @@
+export interface EquipmentData {
+  description: string;
+  id: number;
+}
+
+export interface TagData {
+  id: number;
+  name: string;
+}
+
 export interface TimeData {
   category: string;
   days: number | null;
   hours: number | null;
-  id: string;
+  id: number;
   minutes: number | null;
   note: string | null;
 }
 
 export interface RecipeData {
-  id: string;
+  equipment: EquipmentData[] | null;
+  id: number;
   notes: string | null;
   rating: number | null;
   servings: string | null;
-  tags: { id: string; name: string }[] | null;
+  tags: TagData[] | null;
   times: TimeData[] | null;
   title: string;
 }
